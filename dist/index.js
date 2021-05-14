e598bcdd764e381be998b7232b1612ad8e64358a
Wolly <golliwogg@gmx.de>
2021-05-14 15:34:17 +0200

    added export back again

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,12 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var x = { id: 0 };
-/* import * as A from "./game/common";
-
-export { A };
- */
+var A = __importStar(require("./game/common"));
+exports.A = A;
