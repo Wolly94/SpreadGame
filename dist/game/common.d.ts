f1dbca4f5545a8f8872ac19e8ffcad3b2ed68b53
Wolly <golliwogg@gmx.de>
2021-05-14 15:18:49 +0200

    added common.ts file for game

---
+++ dist/game/common.d.ts
@@ -0,0 +1,5 @@
+export declare const radiusToUnitsFixPoint = 50;
+export declare const radiusToUnits: (radius: number) => number;
+export declare const unitsToRadius: (units: number) => number;
+export declare const unitGrowthRadius = 50;
+export declare const radiusToGrowth: (radius: number) => number;
