a64f30e9363a2c48a39d698d7a7fbcc7733649f7
Wolly <golliwogg@gmx.de>
2021-05-19 08:39:59 +0200

    added export for radiusTo ... calculations

--- src/spreadGame/index.ts
+++ src/spreadGame/index.ts
@@ -2,5 +2,6 @@ import { SpreadGameImplementation } from "./spreadGame";
 import Player from "./player";
 import * as entities from "./entites";
 import * as mapStuff from "./map/index";
+import * as commonStuff from "./common";
 
-export { SpreadGameImplementation, Player, entities, mapStuff };
+export { SpreadGameImplementation, Player, entities, mapStuff, commonStuff };
