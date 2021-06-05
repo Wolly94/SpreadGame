387a1acc803f113b922507561e1184eee5494671
Wolly <golliwogg@gmx.de>
2021-06-05 15:54:58 +0200

    improved formatDescription and added test for it

--- dist/skilltree/utils.js
+++ dist/skilltree/utils.js
@@ -1,13 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var allEqual = function (l) {
+    if (l.length <= 1)
+        return true;
+    else
+        return l[0] === l[1] && allEqual(l.slice(1));
+};
 exports.formatDescription = function (values, formatValue, delimeter) {
-    var result = values.reduce(function (res, val) {
-        if (res.length === 0) {
-            return formatValue(val);
-        }
-        else {
-            return res + delimeter + formatValue(val);
-        }
-    }, "");
-    return result;
+    var formatted = values.map(formatValue);
+    if (allEqual(formatted))
+        return formatted[0];
+    else {
+        var result = formatted.reduce(function (res, val) {
+            if (res.length === 0) {
+                return val;
+            }
+            else {
+                return res + delimeter + val;
+            }
+        }, "");
+        return result;
+    }
 };
