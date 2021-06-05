a0c6d146c2e2385352df32350833227ddc47e26b
Wolly <golliwogg@gmx.de>
2021-06-05 10:32:51 +0200

    added test for preparation cap

--- src/skilltree/perks/preparation.test.ts
+++ src/skilltree/perks/preparation.test.ts
@@ -1,3 +1,4 @@
+import SpreadReplay from "../../messages/replay/replay";
 import { SpreadGameImplementation } from "../../spreadGame";
 import { Preparation } from "./preparation";
 
@@ -6,6 +7,20 @@ test("test preparation", () => {
   const game = SpreadGameImplementation.fromReplay(rep);
   game.runReplay(rep, 5000);
   var clientState = game.toClientGameState();
-  let cell2 = clientState.cells.find((c) => c.id === 1);
+  const cell2 = clientState.cells.find((c) => c.id === 1);
   expect(cell2?.playerId).toBe(1);
+});
+
+test("test preparation cap", () => {
+  const maxLength = 2000000;
+  const rep: SpreadReplay = { ...Preparation.replay, lengthInMs: maxLength };
+  const game = SpreadGameImplementation.fromReplay(rep);
+  game.runReplay(rep, maxLength / 2);
+  var clientState = game.toClientGameState();
+  var cell2 = clientState.cells.find((c) => c.id === 1);
+  const oldValue = cell2 === undefined ? 0 : cell2?.defenderCombatAbilities;
+  game.runReplay(rep, 5000);
+  clientState = game.toClientGameState();
+  cell2 = clientState.cells.find((c) => c.id === 1);
+  expect(oldValue).toBe(cell2?.defenderCombatAbilities);
 });
